93fe5d3c6f30185e26cae36edfa1ccb7fabe7aae
Grace Marsey <gracemarsey@gmail.com>
2022-02-02 11:48:10 +0000

    more of the cart page

--- assets/cart.js
+++ assets/cart.js
@@ -1,49 +1,46 @@
-document.addEventListener(
-  "DOMContentLoaded",
-  function () {
-    const decreaseElements = document.querySelectorAll(".decrease_value");
+$(document).ready(function () {
+  $(".js-example-basic-single").select2();
+  const decreaseElements = document.querySelectorAll(".decrease_value");
 
-    for (let index = 0; index < decreaseElements.length; index++) {
-      const decreaseElement = decreaseElements[index];
+  for (let index = 0; index < decreaseElements.length; index++) {
+    const decreaseElement = decreaseElements[index];
 
-      decreaseElement.addEventListener("click", function () {
-        const itemId = decreaseElement.getAttribute("data-item-id");
+    decreaseElement.addEventListener("click", function () {
+      const itemId = decreaseElement.getAttribute("data-item-id");
 
-        const inputElement = document.querySelector(
-          'input[data-item-id="' + itemId + '"]'
-        );
+      const inputElement = document.querySelector(
+        'input[data-item-id="' + itemId + '"]'
+      );
 
-        if (parseInt(inputElement.value) > 1) {
-          inputElement.value = parseInt(inputElement.value) - 1;
+      if (parseInt(inputElement.value) > 1) {
+        inputElement.value = parseInt(inputElement.value) - 1;
 
-          document.querySelector(".update-button").click();
-        }
-      });
-    }
+        document.querySelector(".update-button").click();
+      }
+    });
+  }
 
-    const increaseElements = document.querySelectorAll(".increase_value");
+  const increaseElements = document.querySelectorAll(".increase_value");
 
-    for (let index = 0; index < increaseElements.length; index++) {
-      const increaseElement = increaseElements[index];
+  for (let index = 0; index < increaseElements.length; index++) {
+    const increaseElement = increaseElements[index];
 
-      increaseElement.addEventListener("click", function () {
-        const itemId = increaseElement.getAttribute("data-item-id");
+    increaseElement.addEventListener("click", function () {
+      const itemId = increaseElement.getAttribute("data-item-id");
 
-        const inputElement = document.querySelector(
-          'input[data-item-id="' + itemId + '"]'
-        );
+      const inputElement = document.querySelector(
+        'input[data-item-id="' + itemId + '"]'
+      );
 
-        if (
-          !parseInt(inputElement.getAttribute("max")) ||
-          parseInt(inputElement.value) <
-            parseInt(inputElement.getAttribute("max"))
-        ) {
-          inputElement.value = parseInt(inputElement.value) + 1;
+      if (
+        !parseInt(inputElement.getAttribute("max")) ||
+        parseInt(inputElement.value) <
+          parseInt(inputElement.getAttribute("max"))
+      ) {
+        inputElement.value = parseInt(inputElement.value) + 1;
 
-          document.querySelector(".update-button").click();
-        }
-      });
-    }
-  },
-  false
-);
+        document.querySelector(".update-button").click();
+      }
+    });
+  }
+}, false);
