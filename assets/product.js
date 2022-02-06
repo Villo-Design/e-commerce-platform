9a5d284a83e555483c75dfd6ea95036105279b54
Grace <grace@Thomass-Mac-mini.local>
2022-02-06 16:59:37 +0000

    Add carousel functionality

---
+++ assets/product.js
@@ -0,0 +1,34 @@
+const imageWidth = 450;
+
+$(document).ready(function () {
+  const leftArrow = document.querySelector(".carousel-arrow.left-arrow");
+  const rightArrow = document.querySelector(".carousel-arrow.right-arrow");
+  const carousel = document.querySelector(".carousel");
+  const carouselMax = parseInt(carousel.getAttribute("data-carousel-max"));
+
+  leftArrow.addEventListener("click", function () {
+    const index = parseInt(carousel.getAttribute("data-carousel-index"));
+
+    if (index > 0) {
+      carousel.setAttribute("data-carousel-index", index - 1);
+
+      carousel.setAttribute(
+        "style",
+        "margin-left: -" + (index - 1) * imageWidth + "px;"
+      );
+    }
+  });
+
+  rightArrow.addEventListener("click", function () {
+    const index = parseInt(carousel.getAttribute("data-carousel-index"));
+
+    if (index < carouselMax - 1) {
+      carousel.setAttribute("data-carousel-index", index + 1);
+
+      carousel.setAttribute(
+        "style",
+        "margin-left: -" + (index + 1) * imageWidth + "px;"
+      );
+    }
+  });
+});
